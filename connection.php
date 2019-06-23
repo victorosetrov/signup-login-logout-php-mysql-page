71e59094c24c6ac9e923cf49b2319334088dd7a1
Viktor Osetrov <victorosetrov@gmail.com>
2019-06-23 13:28:17 +0100

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+    $link = mysqli_connect("localhost", "aliamusina", "n4SRy2J9ejUeKZEM", "c9");
+       if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
